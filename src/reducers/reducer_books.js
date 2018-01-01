991d724f7b2bb6fff0972f0bef1a83ca00bbebb5
cwooz <woosley.christopher@gmail.com>
2018-01-01 17:59:19 -0500

    booksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts 1'},
+    { title: 'Javascript: The Good Parts 2'},
+    { title: 'Javascript: The Good Parts 3'}
+  ]
+}
